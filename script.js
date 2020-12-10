a9c75b002b036a8d1bf2832af309a2da91968d82
JuKad <kadlecovajuliana@gmail.com>
2020-12-10 12:48:58 +0100

    fixing js and customizing top of html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function showTemperature(response){
     let temperature = Math.round(response.data.main.temp);
     let city = response.data.name;
-    let message = 'It is ${temperature} degrees in ${city}';
+    let message = `It is ${temperature} degrees in ${city}`;
     h1.innerHTML = message;
 }
 
 
 let apiKey = "63c0356d5ea58f413b8af4b34fb11290";
 let city = "Tallinn";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
