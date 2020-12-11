05bdea1fd137c599e6e1732e79a68d4e0c83a855
JuKad <kadlecovajuliana@gmail.com>
2020-12-11 14:51:59 +0100

    showing the current temperature

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  let dayIndex = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[dayIndex];
-  let currentDate =document.querySelector("#current-date");
-    currentDate.innerHTML= `${day} ${hours}:${minutes}`;
-}
-
-
-
-function showPosition(position){
-    let apiKey = "63c0356d5ea58f413b8af4b34fb11290";
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = "#current-city"; 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-    let city = response.data.name;
-    axios.get(apiUrl).then(showTemperature);
-}
-
-
 function showTemperature(response){
-    let temperature = Math.round(response.data.main.temp);
+console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let description = document.querySelector("#description");
+let windElement = document.querySelector("#wind");
+let visibilityElement = document.querySelector("#visibility");
+let humidityElement = document.querySelector("#humidity");
+let feelsElement = document.querySelector("#feelsLike");
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+description.innerHTML = response.data.weather[0].description;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+visibilityElement.innerHTML = response.data.wind.speed;
+humidityElement.innerHTML = response.data.main.humidity;
+feelsElement.innerHTML = Math.round(response.data.main.feels_like);
 }
 
-navigator.geolocation.getCurrentPosition(showPosition);
+let apiKey = "63c0356d5ea58f413b8af4b34fb11290";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
