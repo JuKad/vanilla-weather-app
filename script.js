8993f0b1d0354b38298beb8f92e407a18e6a6ba8
JuKad <kadlecovajuliana@gmail.com>
2020-12-11 17:22:13 +0100

    update button2

--- script.js
+++ script.js
@@ -45,8 +45,21 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute ("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
+
+function search(city){
 let apiKey = "63c0356d5ea58f413b8af4b34fb11290";
-let city = "Moscow";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
+
+}
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
