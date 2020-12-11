a2817a9f81e1543316c5ccf78c8954d7e4a6aa32
JuKad <kadlecovajuliana@gmail.com>
2020-12-11 15:48:34 +0100

    update time

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp){
+let date= new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+  }
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+let day = days[date.getDay()];
+return`${day} ${hours}:${minutes} `;
+}
+
+
+
 function showTemperature(response){
 console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
@@ -7,6 +32,7 @@ let windElement = document.querySelector("#wind");
 let visibilityElement = document.querySelector("#visibility");
 let humidityElement = document.querySelector("#humidity");
 let feelsElement = document.querySelector("#feelsLike");
+let dateElement = document.querySelector("#date");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 description.innerHTML = response.data.weather[0].description;
@@ -14,6 +40,7 @@ windElement.innerHTML = Math.round(response.data.wind.speed);
 visibilityElement.innerHTML = response.data.wind.speed;
 humidityElement.innerHTML = response.data.main.humidity;
 feelsElement.innerHTML = Math.round(response.data.main.feels_like);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "63c0356d5ea58f413b8af4b34fb11290";
